da97a88e97c313273bd0fc9e7142a11e3ea5dd7a
Angella Trujillo <angijankylcar@gmail.com>
2025-11-10 21:47:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4652577095-9f1a2",
+  "appId": "1:610961279511:web:c73658e1e7a814bfbf2a52",
+  "apiKey": "AIzaSyCeKbrpDq7rhOP7umOeXRoSoZ8iVngCzwc",
+  "authDomain": "studio-4652577095-9f1a2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "610961279511"
+};
