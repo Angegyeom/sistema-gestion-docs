2cb65defd387824bd22d29199d89c299ba9e824d
Angella Trujillo <angijankylcar@gmail.com>
2025-11-10 21:54:11 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { FirebaseClientProvider } from "@/firebase/client-provider";
 
 export default function PublicLayout({
@@ -8,28 +7,5 @@ export default function PublicLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="es">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body
-        className={cn(
-          "font-sans antialiased",
-          "min-h-screen bg-background"
-        )}
-      >
-        <FirebaseClientProvider>{children}</FirebaseClientProvider>
-      </body>
-    </html>
-  );
+  return <FirebaseClientProvider>{children}</FirebaseClientProvider>;
 }
